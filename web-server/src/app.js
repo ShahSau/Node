48d5d330a652b6ad458aeb5edf96c0b9aca57aed
ShahSau <shahriarksaurov@gmail.com>
2021-12-29 09:56:21 +0200

    update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,38 @@
-const path = require("path")
-const express = require("express")
+const path = require("path");
+const express = require("express");
+const hbs =require("hbs")
+const app = express();
+//defie paths for express config
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+// setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath)
+// setup static directory
+app.use(express.static(path.join(__dirname, "../public")));
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather app",
+    name: "shah",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    title: "about hbs",
+    name:"shah"
+  });
+});
 
+app.get("/help", (req, res) => {
+  res.render("help.hbs", {
+    title: "help",
+    name:'shah',
+    message: "help me!!",
+  });
+});
 
-const app =express()
-app.use(express.static(path.join(__dirname, "../public")))
-
-app.get("/about", (req,res)=>{
-    res.sendFile( path.join(__dirname, "../public" ,"about.html"))
-})
-
-app.get("/help", (req,res)=>{
-    res.sendFile( path.join(__dirname, "../public" ,"help.html"))
-})
-
-app.listen(3000, ()=>{
-    console.log(`server running`)
-})
-
+app.listen(3000, () => {
+  console.log(`server running`);
+});
